e078f4e085a08591a8d7fff23c4e0d0b6f4abf2f
Yuta <ymmt7974@gmail.com>
2021-01-24 01:23:41 +0900

    Doctrine ORM【後編】

--- src/AppBundle/Controller/OmikujiController.php
+++ src/AppBundle/Controller/OmikujiController.php
@@ -36,4 +36,150 @@ class OmikujiController extends Controller  // ①
             'unsei' => $omikuji[$number],
         ]);
     }
+
+    /**
+     * @Route("/find")
+     */
+    public function findAction()
+    {
+        /**
+         * エンティティの検索はリポジトリを通して行う ①
+         * @var UnseiRepository $repository 
+         */
+        $repository = $this->getDoctrine()->getRepository(Unsei::class);
+
+        // 全て検索 ②
+        $unseis = $repository->findAll();
+        dump($unseis);
+        
+        // ID(プライマリキー)で検索 ③
+        $unsei = $repository->find(1);
+        dump($unsei);
+
+        // 複数の項目で１件だけ検索（ここでは'name'だけですが...）④
+        $unsei = $repository->findOneBy([
+            'name' => '大吉',
+        ]);
+        dump($unsei);
+        
+        // 複数の項目で複数件検索（ここでは'name'だけですが...）⑤
+        // ※ 配列が返ってくる
+        $unsei = $repository->findBy([
+            'name' => '大吉',
+        ]);
+        dump($unsei);
+        
+        // プロパティに対応したダイナミックメソッドを使って１件だけ検索 ⑥
+        $unsei = $repository->findOneById(1);
+        dump($unsei);
+        $unsei = $repository->findOneByName('中吉');
+        dump($unsei);
+        
+        // ダイナミックメソッドを使って複数件検索 ⑦
+        $unsei = $repository->findByName('中吉');
+        dump($unsei);
+        
+        die; // プログラムを終了して、dumpを画面に表示 ⑧
+        return new Response("Dummy");
+    }
+
+    /**
+     * @Route("/crud")
+     */
+    public function crudAction()
+    {
+        /**
+         * エンティティの作成、更新、削除はエンティティマネージャを通して行う ①
+         *  @var EntityManager $em
+         */
+        $em = $this->getDoctrine()->getManager();
+
+        //
+        // Create
+        //
+        $unsei = new Unsei();    // ②
+        $unsei->setName("大凶");
+        dump($unsei);
+
+        $em->persist($unsei);    // ③
+        $em->flush();            // ④
+        dump($unsei);
+
+        //
+        // Read ⑤
+        //
+        $repository = $em->getRepository(Unsei::class);
+        
+        /** @var Unsei $unsei */
+        $unsei = $repository->findOneByName('大凶'); // ⑥
+        dump($unsei);
+        
+        //
+        // Update ⑦
+        //
+        $unsei->setName("大大吉");
+        $em->flush();
+        dump($unsei);
+        
+        $unsei = $repository->find($unsei->getId());
+        dump($unsei);
+        
+        // 
+        // Delete ⑧
+        // 
+        $em->remove($unsei);
+        $em->flush();
+        
+        $unseis = $repository->findAll();
+        dump($unseis);
+        foreach ($unseis as $unsei) {
+            dump($unsei->getName());
+        }
+        
+        die; // プログラムを終了して、dumpを画面に表示
+
+        return new Response("Dummy");
+    }
+
+    /**
+     * @Route("/dql")
+     */
+    public function dql()
+    {
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            'SELECT u
+            FROM AppBundle:Unsei u
+            WHERE u.name = :name'        // ①
+        )->setParameter('name', '大吉');
+        
+        $unsei = $query->getResult();
+        dump($unsei);
+        
+        die; // プログラムを終了して、dumpを画面に表示
+
+        return new Response("Dummy");
+    }
+
+    /**
+     * @Route("/qb")
+     */
+    public function queryBuilder()
+    {
+        /** @var UnseiRepository $repository **/
+        $repository = $this->getDoctrine()->getRepository(Unsei::class);
+        
+        $query = $repository->createQueryBuilder('u')
+            ->where('u.name = :name')
+            ->setParameter('name', '大吉')
+            ->getQuery();
+
+        $unsei = $query->getResult();
+        dump($unsei);
+        
+        die; // プログラムを終了して、dumpを画面に表示
+
+        return new Response("Dummy");
+    }
 }
